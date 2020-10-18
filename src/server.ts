2271649be3ea4a02c712caf86f9ab88a452dc9ec
Nelson Jr <juniordell123@gmail.com>
2020-10-17 23:25:50 -0300

    update Server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import express from "express";
 
 import "./database/connections";
 
+import routes from "./routes";
+
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {});
+app.use(routes);
 
 app.listen(3333);
